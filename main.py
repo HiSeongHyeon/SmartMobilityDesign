d2529b53e1d6ebe87f7b7ba070a3004f8e97b356
KimchiPizza <min__369@naver.com>
2025-06-21 03:28:12 +0900

    docs: 주석 수정

--- main.py
+++ main.py
@@ -10,20 +10,17 @@ from config import crosswalk_completed
 from sensor import Camera, Lidar
 import numpy as np
 
-# obs or tunnel or crosswalk or stopline
 
 
 if __name__ == '__main__':
     rospy.init_node('auto_drive')
     camera = Camera()
-    #camera.crosswalk_completed = True
-    #crosswalk_completed = True
     lidar = Lidar()
     control = XycarControl()
     control.init_publisher()
 
 
-    # 첫 이미지 수신 대기
+    # 첫 이미지 및 라이다 수신 대기
     while not rospy.is_shutdown() and (np.sum(camera.raw_image) == 0 or lidar.lidar_points is None):
         rospy.sleep(0.1)
 
@@ -42,7 +39,7 @@ if __name__ == '__main__':
             left, right = lidar.tunnel_driving()
             angle = control.tunnel_PID(left, right)
             control.drive(angle, 5)
-            continue
+            continue   #continue 문을 통해 일반 직선 및 곡선 구간, 횡단보도 구간, 정지선 구간 판단 로직 스킵
 
         # 장애물 구간 제어
         elif is_obs:
@@ -53,8 +50,6 @@ if __name__ == '__main__':
                     angle = -control.obstacle_PID(distance, theta)
                 else:
                     angle = control.obstacle_PID(distance, theta)
-                # print("avg distance and theta = ", distance, theta)
-                # print("left angle = ", angle)
                 control.drive(angle, 5)
 
             if is_obs == 2:
@@ -64,15 +59,13 @@ if __name__ == '__main__':
                     angle = control.obstacle_PID(distance, theta)
                 else:
                     angle = -control.obstacle_PID(distance, theta)
-                # print("avg distance and theta = ", distance, theta)
-                # print("right angle = ", angle)
                 control.drive(angle, 5)
-            continue
+            continue    #continue 문을 통해 일반 직선 및 곡선 구간, 횡단보도 구간, 정지선 구간 판단 로직 스킵
 
-        # 일반 직선 곡선 구간, 횡단보도 구간, 정지선 구간 판단.
+        # 일반 직선 및 곡선 구간, 횡단보도 구간, 정지선 구간 판단.
         lpos, rpos, is_crosswalk, is_stopline = camera.process_calibration_and_birdeye()
 
-
+        # 횡단보도 구간 제어
         if(is_crosswalk and not crosswalk_completed):
             print"========== this is crosswalk ========="
             control.drive(0, 0)
@@ -80,15 +73,14 @@ if __name__ == '__main__':
             crosswalk_completed = True
             camera.crosswalk_completed = crosswalk_completed
 
-
-
+        # 정지선 구간 제어
         elif(crosswalk_completed and is_stopline):
             print"========= this is stopline ========="
             control.drive(0, 0)
             time.sleep(1)
-
             break
 
+        # 일반 직선 및 곡선 구간 제어
         else:
             print"========= this is general ========="
             center = (lpos + rpos) / 2
